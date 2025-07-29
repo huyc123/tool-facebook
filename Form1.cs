efb684b27aac60f2cfb809fdc3aac7170f87852c
nstanawa <langthambca@gmail.com>
2025-07-29 22:50:34 +0700

    khoi tao du an va chuc nang cmt bai post

---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace selenium66
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var options = new ChromeOptions();
+            var chromeDriverService = ChromeDriverService.CreateDefaultService();
+            chromeDriverService.HideCommandPromptWindow = true;
+
+            options.AddArgument("--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Mobile/15E148 Safari/604.1");
+            //            //options.AddArgument("--mute-audio");
+            //            //options.AddArgument("--disable-extensions");
+            options.AddArgument("--window-size=530,1000");
+            //            //options.AddArgument("--disable-notifications");
+
+            var Chromedriver = new ChromeDriver(chromeDriverService, options);
+
+            var wait = new WebDriverWait(Chromedriver, TimeSpan.FromSeconds(10));
+            Chromedriver.Navigate().GoToUrl("https://m.facebook.com/login/");
+
+
+            var element1 = Chromedriver.FindElements(By.XPath("/html/body/div[1]/div/div[2]/div[1]/div/div/div/div/div/div[1]/div/div/div/div[3]/div/div/div/div[2]/div/div/div/div/div/div[1]/div[2]/div"));
+            //var element2 = Chromedriver.FindElements(By.XPath("//xpath_thu_2"));
+
+            if (element1.Count > 0 && element1[0].Displayed)
+            {
+                element1[0].Click();
+            }
+            //else if (element2.Count > 0 && element2[0].Displayed)
+            //{
+            //    element2[0].Click();
+            //}
+            else
+            {
+                // Không có phần tử nào -> tiếp tục chạy chương trình
+
+
+
+
+
+                var email = Chromedriver.FindElement(By.Id("m_login_email"));
+                email.SendKeys("0967814637");
+
+                //            // Tìm và nhập mật khẩu
+                var password = Chromedriver.FindElement(By.Id("m_login_password"));
+
+                password.SendKeys("290962328");
+
+                //            // Nhấn nút đăng nhập
+                var loginButton = Chromedriver.FindElement(By.XPath("/html/body/div[1]/div/div[2]/div[1]/div/div/div/div/div/div[1]/div/div/div/div[3]/div/div/div/div/div/div/div/div[2]/div[3]/div[3]/div/div/div/div/div/div/div/div/div/span"));
+                loginButton.Click();
+
+
+                   nThread.Sleep(5000);
+
+                //Chromedriver.Navigate().GoToUrl("https://www.facebook.com/bong.teen/posts/pfbid0EpsaM2KwrkN4VyFVJVx4dVvqbXgks4ppJeXHfhvZKTWms6mLFCeGG3QaZoKupD1bl");
+                //Chromedriver.Navigate().GoToUrl("https://www.facebook.com/story.php?story_fbid=1173509351469154&id=100064302996623");
+
+
+                Chromedriver.Navigate().GoToUrl("https://www.facebook.com/story.php?story_fbid=1231673242305493&id=100063884875945");
+
+
+
+                Thread.Sleep(5000);
+                //bamvaocmt
+
+
+                string nutanvao = "/html/body/div[3]/div/div[1]/div[10]/div[5]/div/div[3]";
+                var XPathnutanvao = wait.Until(driver => driver.FindElement(By.XPath(nutanvao)));
+
+                //// Cuộn phần tử vào giữa màn hình
+                ((IJavaScriptExecutor)Chromedriver).ExecuteScript("arguments[0].scrollIntoView({block: 'center'});", XPathnutanvao);
+                Thread.Sleep(500); // Chờ một chút sau khi scroll
+
+                // Cách click an toàn bằng JavaScript (bỏ qua phần tử che)
+                ((IJavaScriptExecutor)Chromedriver).ExecuteScript("arguments[0].click();", XPathnutanvao);
+
+
+
+
+
+                Thread.Sleep(2000);
+                string xpathCmtNoiDung = "/html/body/div[3]/div/div[4]/div/div/div[1]/div[3]/div[2]/div/div[1]/textarea";
+                var xpathCmtNoiDungcmt = wait.Until(driver => driver.FindElement(By.XPath(xpathCmtNoiDung)));
+                xpathCmtNoiDungcmt.SendKeys("Còn không chị ơi");
+
+                string xpathCmtgui = "/html/body/div[3]/div/div[4]/div/div/div[1]/div[3]/div[2]/div/div[8]/div[1]/div/div[2]/div/div";
+                var cmtxpathCmtgui = wait.Until(driver => driver.FindElement(By.XPath(xpathCmtgui)));
+                cmtxpathCmtgui.Click();
+
+            }
+        }
+    }
+}
+
+
+// bước 1 xác định vị trí lỗi : có tên file và dòng 48
+// bước 2 xác định lỗi : coppy dịch sang tiếng việt xem nó lỗi gì
+// nếu hiểu tự sửa chưa dùng AI
+// sửa mà không chạy hỏi GPT, yêu cầu nó giải thích từng dòng code, luồng chạy
+// tốt nhất e mới nên gõ lại không nên coppy paste -> lý do: e mới nên khoogn nhớ được, ta đang hocjkhoong phải mục tiêu bất chấp làm ra sản phẩm
+
+// đọc thì không hiểu lắm đúng không? đại loại là bool -> kiểu dữ liệu true false> nghi ngờ kiểu dữ liệu bị sai.
+
